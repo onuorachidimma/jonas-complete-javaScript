1bdedb90532f62cd95da87504faf33006bb9db39
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-05-17 00:27:24 +0100

    Initial commit

---
+++ 01-Fundamentals-Part-1/assignment.js
@@ -0,0 +1,8 @@
+let country = "Nigeria";
+let continent = "Africa";
+let population = 120000000;
+
+console.log(country)
+console.log(continent)
+console.log(population)
+console.log(`I am from ${country} in ${continent}. We have a population of ${population} citizens`)
---
+++ 01-Fundamentals-Part-1/script.js
@@ -0,0 +1,5 @@
+const js = "amazing"
+
+if (js === "amazing") alert("JavaScript is FUN!");
+
+console.log(2 + 3 / 3 + 4 + 3)
