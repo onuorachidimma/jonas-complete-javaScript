237a6b12319620bfa29d72e54633a845006aa15e
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-05-17 21:41:37 +0100

    variable declaration, basicors operat

--- 01-Fundamentals-Part-1/assignment.js
+++ 01-Fundamentals-Part-1/assignment.js
@@ -1,9 +1,10 @@
-// ######### VALUES AND VARIABLES
+// ######### VALUES AND VARIABLES #############
 
+/*
 let country = "Nigeria";
 let continent = "Africa";
 let population = 120000000;
-/*
+
 console.log(country)
 console.log(continent)
 console.log(population)
@@ -12,12 +13,39 @@ console.log(`I am from ${country} in ${continent}. We have a population of ${pop
 
 
 
-// ######### VALUES AND VARIABLES
+// ######### DATA TYPES ##############
 
-let isIsland = false;
+/*
+const isIsland = false;
 let language;
 
 console.log(typeof isIsland)
 console.log(typeof population)
 console.log(typeof country)
 console.log(typeof language)
+console.log(`It is ${isIsland} that ${country} is an Island. `)
+*/
+
+// ######### LET, CONST, VAR ##############
+
+
+const language = "English"
+const country = "Nigeria";
+const continent = "Africa";
+let population = 120000000;
+
+
+
+// ######### BASIC OPERATORS ##############
+let halfPopulation = population / 2;
+console.log(population++);
+let finlandPopulation = 6000000
+console.log(population > finlandPopulation)
+let aveeragePopulation = 33000000
+console.log(population < aveeragePopulation)
+
+const description = country + " is in " + continent + ", " + "and its " + population + " people speak " + language
+
+console.log(description)
+
+
