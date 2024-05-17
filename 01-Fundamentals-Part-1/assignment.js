96a8c68b06a40d33fdb735290ae05d75b7d249b3
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-05-17 01:02:31 +0100

    values and variables

--- 01-Fundamentals-Part-1/assignment.js
+++ 01-Fundamentals-Part-1/assignment.js
@@ -1,8 +1,23 @@
+// ######### VALUES AND VARIABLES
+
 let country = "Nigeria";
 let continent = "Africa";
 let population = 120000000;
-
+/*
 console.log(country)
 console.log(continent)
 console.log(population)
 console.log(`I am from ${country} in ${continent}. We have a population of ${population} citizens`)
+*/
+
+
+
+// ######### VALUES AND VARIABLES
+
+let isIsland = false;
+let language;
+
+console.log(typeof isIsland)
+console.log(typeof population)
+console.log(typeof country)
+console.log(typeof language)
