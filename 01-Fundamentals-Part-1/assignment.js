5978fe49e7b60d62916cbf6132d97ae687e4c107
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-05-18 16:47:18 +0100

    if/else assignment done

--- 01-Fundamentals-Part-1/assignment.js
+++ 01-Fundamentals-Part-1/assignment.js
@@ -41,11 +41,39 @@ let halfPopulation = population / 2;
 console.log(population++);
 let finlandPopulation = 6000000
 console.log(population > finlandPopulation)
-let aveeragePopulation = 33000000
-console.log(population < aveeragePopulation)
+let averagePopulation = 33000000
+console.log(population < averagePopulation)
 
-const description = country + " is in " + continent + ", " + "and its " + population + " people speak " + language
+const description1 = country + " is in " + continent + ", " + "and its " + population + " people speak " + language
 
-console.log(description)
+console.log(description1)
 
+// ######### STRINGS AND TEMPLATE LITERALS ##############
+const description2 = `${country} is in ${continent}, and its ${population} people speak ${language}`
+
+console.log(description2)
+
+// ######### IF/ELSE STATEMENTS ##############
+
+const averagePopulationNow = 33000000 - population;
+
+if (population > 33000000){
+    console.log(`${country}'s population is ${averagePopulationNow} below average`)
+}else{
+    console.log(`${country}'s population is ${averagePopulationNow} above average`)
+}
+
+if (population > 33000000){
+    population = 13000000
+    console.log(`${country}'s population is ${averagePopulationNow} below average`)
+}else{
+    console.log(`${country}'s population is ${averagePopulationNow} above average`)
+}
+
+if (population > 33000000){
+    population = 130000000
+    console.log(`${country}'s population is ${averagePopulationNow} below average`)   
+}else{
+    console.log(`${country}'s population is ${averagePopulationNow} above average`)
+}
 
