ec65dfc33abd0b8cc3c99bd0d777a7cc36514f41
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-05-18 16:04:02 +0100

    challenge #2 done

--- 01-Fundamentals-Part-1/challenge.jsx
+++ 01-Fundamentals-Part-1/challenge.jsx
@@ -1,18 +1,37 @@
 /* Write your code below. Good luck! 🙂 */
-let massMark, massJohn, heightMark, heightJohn
-massMark = 78
-heightMark = 1.69
-massJohn = 92
-heightJohn = 1.95
+
+// CHALENGE #1
+
+// ### Test 2 ###
+// const massMark = 78
+// const heightMark = 1.69
+// const massJohn = 92
+// const heightJohn = 1.95
+
+// ### Test 2 ###
+const massMark = 95
+const heightMark = 1.88
+const massJohn = 85
+const heightJohn = 1.76
 const BMIMark = massMark / (heightMark * heightMark) 
 const BMIJohn = massJohn / (heightJohn * heightJohn) 
-console.log(BMIMark, BMIJohn)
-massMark = 95
-heightMark = 1.88
-massJohn = 85
-heightJohn = 1.76
-
 console.log(BMIMark, BMIJohn);
 
 const markHigherBMI = BMIMark > BMIJohn
 console.log(markHigherBMI);
+
+
+// CHALLENGE #2
+/* Write your code below. Good luck! 🙂 */
+
+if (BMIMark > BMIJohn) {
+    console.log("Mark's BMI is higher than John's!")
+} else{
+    console.log("John's BMI is higher than Mark's!")
+}
+
+if (BMIMark > BMIJohn) {
+    console.log(`Mark's BMI (${BMIMark}) is higher than John's (${BMIJohn}!)`)
+} else{
+    console.log(`John's BMI (${BMIJohn}) is higher than Mark's (${BMIMark}!)`)
+}
--- 01-Fundamentals-Part-1/challengeInstruction.md
+++ 01-Fundamentals-Part-1/challengeInstruction.md
@@ -25,3 +25,20 @@ Your task is to write some code to help them:
 👋 **OPTIONAL:** You can watch my solution in video format in the next lecture.
 
 **IMPORTANT:** The `**` operator is not supported in this editor. Please make sure to use exactly this formula `mass / (height * height)`, and not this one `mass / (height ** 2)`.
+
+
+# CHALLENGE #2
+
+Use the BMI example from Challenge #1, and the code you already wrote, and improve it:
+
+1. Print a nice output to the console, telling the user who has the higher BMI. The message can be either:
+   - "Mark's BMI is higher than John's!" 
+   - "John's BMI is higher than Mark's!".
+
+2. Modify the outputs above to use template literals to include the BMI values in the outputs.
+
+   Example:
+   - "Mark's BMI (28.3) is higher than John's (23.9)!"
+   - "John's BMI (29.1) is higher than Mark's (27)!"
+
+**Note:** Don't round the BMI values. Leave them as they are.
