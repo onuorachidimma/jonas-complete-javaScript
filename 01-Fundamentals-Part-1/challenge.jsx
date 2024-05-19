6c9f68d34348bd3728b10e7bd5daf6756f82ab5f
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-05-20 00:05:04 +0100

    challenge #3

--- 01-Fundamentals-Part-1/challenge.jsx
+++ 01-Fundamentals-Part-1/challenge.jsx
@@ -45,6 +45,6 @@ if (scoreDolphins > scoreKoalas){
     console.log("Dolphins win the trophy")
 }else if(scoreKoalas > scoreDolphins){
     console.log("Koalas win the trophy")
-}else{
+}else if (scoreDolphins === scoreKoalas){
     console.log("Both win the trophy")
 }
