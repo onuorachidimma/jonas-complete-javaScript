70773592c801513c1356d5087ea0358f45f34d1c
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-05-20 00:51:43 +0100

    assignment on switch statements done

--- 01-Fundamentals-Part-1/assignment.js
+++ 01-Fundamentals-Part-1/assignment.js
@@ -98,3 +98,27 @@ if ( language === "English" && population < 50000000 && isIsland){
 }else{
     console.log(` ${country} does not meet your criteria :(`)
 }
+
+// ######### SWITCH STATEMENT ##############
+const switchLanguage = "German"
+
+switch (switchLanguage){
+    case "chinese":
+    case "mandarin":
+        console.log('MOST number of native speakers!')
+        break;
+    case "spanish":
+        console.log('2nd place in number of native speakers')
+        break;
+    case "english":
+        console.log('2rd place')
+        break;
+    case "hindi":
+        console.log('Number 4')
+        break;
+    case "arabic":
+        console.log('5th most spoken language')
+        break;
+        default:
+            console.log('Great language too :D')
+}
