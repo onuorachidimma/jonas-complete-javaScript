dd489d20de6bfe531556127f918248826e001fac
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-05-20 19:23:08 +0100

    function declaration/expression assignment done

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -1,6 +1,6 @@
 'use strict'
 
-// ########## FUNCTIONS ASSSIGNMENT ###########
+// ########## FUNCTIONS  ###########
 function describeCountry (country, population, capitalCity){
     return `${country} has ${population} people and its capital city is ${capitalCity}`
 }
@@ -9,3 +9,28 @@ const nigera = describeCountry("Nigeria", 120000000, "Abuja");
 const angola = describeCountry("Angola", 10000000, "Luanda");
 const germany = describeCountry("Germany", 100000, "Berlin");
 console.log(nigera, angola, germany)
+
+
+// ########## FUNCTION DECLARATION VS EXPRESSIONS  ###########
+// Function declaration
+function percentageOfWorld1(country, population){
+    return `${country} has ${population} people, so it's about ${(population / 7900) * 100}% of the world population`
+}
+const nigeriaPopulation = percentageOfWorld1("Nigeria", 120000)
+console.log(nigeriaPopulation)
+const chinaPopulation = percentageOfWorld1("China", 1441)
+console.log(chinaPopulation)
+const germanyPopulation = percentageOfWorld1("Germany", 100000)
+console.log(germanyPopulation)
+
+// Function expression
+const percentageOfWorld2 = function(country, population){
+    return `${country} has ${population} people, so it's about ${(population / 7900) * 100}% of the world population`
+}
+
+const nigeriaPopulationExp = percentageOfWorld2("Nigeria", 120000)
+console.log(nigeriaPopulationExp)
+const chinaPopulationExp = percentageOfWorld2("China", 1441)
+console.log(chinaPopulationExp)
+const germanyPopulationExp = percentageOfWorld2("Germany", 100000)
+console.log(germanyPopulationExp)
