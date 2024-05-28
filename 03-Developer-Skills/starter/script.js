3fd8f9730d2a05bc98aa527c69842146cb8a11f2
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-05-28 23:23:07 +0100

    challenge#1 instructions

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,17 +1,30 @@
 // Remember, we're gonna use strict mode in all scripts now!
 "use strict";
 
-// ######## CHALLENGE #1 ##########
+///////////////////////////////////////
+// Coding Challenge #1
+
+/*
+Given an array of forecasted maximum temperatures, the thermometer displays a string with these temperatures.
+
+Example: [17, 21, 23] will print "... 17ºC in 1 days ... 21ºC in 2 days ... 23ºC in 3 days ..."
+
+Create a function 'printForecast' which takes in an array 'arr' and logs a string like the above to the console.
+
+Use the problem-solving framework: Understand the problem and break it up into sub-problems!
+
+TEST DATA 1: [17, 21, 23]
+TEST DATA 2: [12, 5, -5, 0, 4]
+*/
 
 const printForcast = (arr) => {
   console.log(arr);
-   let forcast = '';
+  let forcast = "";
   for (let i = 0; i < arr.length; i++) {
-    forcast += `... ${arr[i]}*C in ${i + 1} days`
+    forcast += `... ${arr[i]}*C in ${i + 1} days`;
   }
   return forcast;
 };
-
 // const theForcast = printForcast([17, 21, 23]);
 const theForcast = printForcast([12, 5, -5, 0, 4]);
 console.log(theForcast);
