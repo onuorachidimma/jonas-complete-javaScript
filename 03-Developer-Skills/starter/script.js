4338626065683f44142bc870bdcc2bad53c4888a
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-05-28 23:18:02 +0100

    challenge #1 for dev skills and setup done

--- 03-Developer-Skills/starter/script.js
+++ 03-Developer-Skills/starter/script.js
@@ -1,3 +1,17 @@
 // Remember, we're gonna use strict mode in all scripts now!
-'use strict';
+"use strict";
 
+// ######## CHALLENGE #1 ##########
+
+const printForcast = (arr) => {
+  console.log(arr);
+   let forcast = '';
+  for (let i = 0; i < arr.length; i++) {
+    forcast += `... ${arr[i]}*C in ${i + 1} days`
+  }
+  return forcast;
+};
+
+// const theForcast = printForcast([17, 21, 23]);
+const theForcast = printForcast([12, 5, -5, 0, 4]);
+console.log(theForcast);
