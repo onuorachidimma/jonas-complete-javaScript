2b6f9ed8bb82efc11d5c900561965c562ae757b3
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-05-28 08:35:59 +0100

    assignment on arrays done

--- 02-Fundamentals-Part-2/challenges.js
+++ 02-Fundamentals-Part-2/challenges.js
@@ -1,26 +1,25 @@
 /* Write your code below. Good luck! 🙂 */
 
-const calcAverage1 = (num1, num2, num3) =>{
-    return (num1 + num2 + num3)/ 3
-}
+const calcAverage1 = (num1, num2, num3) => {
+  return (num1 + num2 + num3) / 3;
+};
 
 // const scoreDolphins = calcAverage(44, 23, 71)
 // const scoreKoalas = calcAverage(65, 54, 49)
-const scoreDolphins = calcAverage(85, 54, 41)
-const scoreKoalas = calcAverage(23, 34, 27)
+const scoreDolphins = calcAverage(85, 54, 41);
+const scoreKoalas = calcAverage(23, 34, 27);
 
-const checkWinner = (avgDolphins, avgKoalas) =>{
-    if (avgDolphins >= avgKoalas * 2){
-        console.log(`Dolphins win (${scoreDolphins } vs. ${scoreKoalas})`)
-    }else if(avgKoalas >= avgDolphins * 2){
-        console.log(`Koala win (${scoreKoalas} vs. ${scoreDolphins})`)
-    }else{
-        console.log(`No team wins...`)
-    }
-}
+const checkWinner = (avgDolphins, avgKoalas) => {
+  if (avgDolphins >= avgKoalas * 2) {
+    console.log(`Dolphins win (${scoreDolphins} vs. ${scoreKoalas})`);
+  } else if (avgKoalas >= avgDolphins * 2) {
+    console.log(`Koala win (${scoreKoalas} vs. ${scoreDolphins})`);
+  } else {
+    console.log(`No team wins...`);
+  }
+};
 
-checkWinner(scoreDolphins, scoreKoalas)
-
+checkWinner(scoreDolphins, scoreKoalas);
 
 // ########  CHALLENGE #2 ###########
 
@@ -44,59 +43,63 @@ checkWinner(scoreDolphins, scoreKoalas)
 // const totals = [bills[0] + tips[0], bills[1] + tips[1], bills[2] + tips[2]]
 // console.log(totals)
 
-
-
 // ########  CHALLENGE #3 ###########
 /* Write your code below. Good luck! 🙂 */
 
 const mark = {
-    fullName: "Mark Miller",
-    mass: 78,
-    height: 1.69,
-    calcBMI: function(){
-       this.bmi = this.mass / (this.height * this.height) 
-       return this.bmi
-    }
-}
-mark.calcBMI()
+  fullName: "Mark Miller",
+  mass: 78,
+  height: 1.69,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+mark.calcBMI();
 
 const john = {
-    fullName: "John Smith",
-    mass: 92,
-    height: 1.95,
-    calcBMI: function(){
-       this.bmi = this.mass / (this.height * this.height) 
-       return this.bmi
-    }
-}
-john.calcBMI()
+  fullName: "John Smith",
+  mass: 92,
+  height: 1.95,
+  calcBMI: function () {
+    this.bmi = this.mass / (this.height * this.height);
+    return this.bmi;
+  },
+};
+john.calcBMI();
 
-const comp = mark.calcBMI() > john.calcBMI()
-console.log(comp ? `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})` : `${john.fullName}'s BMI (${john.bmi} is higher than ${mark.fullName}'s (${mark.bmi})` )
+const comp = mark.calcBMI() > john.calcBMI();
+console.log(
+  comp
+    ? `${mark.fullName}'s BMI (${mark.bmi}) is higher than ${john.fullName}'s (${john.bmi})`
+    : `${john.fullName}'s BMI (${john.bmi} is higher than ${mark.fullName}'s (${mark.bmi})`
+);
 
 // ########  CHALLENGE #4 ###########
 
 const calcTip = function (bill) {
-    return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
-  }
-  
-  /* Write your code below. Good luck! 🙂 */
-  const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52]
-  const tips = []
-  const totals = []
-  
-  for (let i = 0; i < bills.length; i++){
-      tips.push(calcTip(bills[i]))
-      totals.push(bills[i] + tips[i])
-  }
-  console.log(tips)
-  console.log(totals)
+  return bill >= 50 && bill <= 300 ? bill * 0.15 : bill * 0.2;
+};
 
-  const calcAverage = (arr) => {
-    let sum = 0;
-    for(let i = 0; i < arr.length; i++){
-        sum += arr[i]
-    }
-    return sum / arr.length
+/* Write your code below. Good luck! 🙂 */
+const bills = [22, 295, 176, 440, 37, 105, 10, 1100, 86, 52];
+const tips = [];
+const totals = [];
+
+for (let i = 0; i < bills.length; i++) {
+  tips.push(calcTip(bills[i]));
+  totals.push(bills[i] + tips[i]);
 }
-console.log(calcAverage(totals))
+console.log(tips);
+console.log(totals);
+
+const calcAverage = (arr) => {
+  let sum = 0;
+  for (let i = 0; i < arr.length; i++) {
+    sum += arr[i];
+  }
+  return sum / arr.length;
+};
+console.log(calcAverage(totals));
+
+
--- 02-Fundamentals-Part-2/script.js
+++ 02-Fundamentals-Part-2/script.js
@@ -1,64 +1,64 @@
-'use strict'
+"use strict";
 
 // ########## FUNCTIONS  ###########
-function describeCountry (country, population, capitalCity){
-    return `${country} has ${population} people and its capital city is ${capitalCity}`
+function describeCountry(country, population, capitalCity) {
+  return `${country} has ${population} people and its capital city is ${capitalCity}`;
 }
 
 const nigera = describeCountry("Nigeria", 120000000, "Abuja");
 const angola = describeCountry("Angola", 10000000, "Luanda");
 const germany = describeCountry("Germany", 100000, "Berlin");
-console.log(nigera, angola, germany)
-
+console.log(nigera, angola, germany);
 
 // ########## FUNCTION DECLARATION VS EXPRESSIONS  ###########
 // Function declaration
-function percentageOfWorld1(population){
-    return population / 7900 * 100
+function percentageOfWorld1(population) {
+  return (population / 7900) * 100;
 }
-const nigeriaPopulation = percentageOfWorld1(120000)
-console.log(nigeriaPopulation)
-const chinaPopulation = percentageOfWorld1(1441)
-console.log(chinaPopulation)
-const germanyPopulation = percentageOfWorld1(100000)
-console.log(germanyPopulation)
+const nigeriaPopulation = percentageOfWorld1(120000);
+console.log(nigeriaPopulation);
+const chinaPopulation = percentageOfWorld1(1441);
+console.log(chinaPopulation);
+const germanyPopulation = percentageOfWorld1(100000);
+console.log(germanyPopulation);
 
 // Function expression
-const percentageOfWorld2 = function(country, population){
-    return `${country} has ${population} people, so it's about ${(population / 7900) * 100}% of the world population`
-}
+const percentageOfWorld2 = function (country, population) {
+  return `${country} has ${population} people, so it's about ${
+    (population / 7900) * 100
+  }% of the world population`;
+};
 
-const nigeriaPopulationExp = percentageOfWorld2("Nigeria", 120000)
-console.log(nigeriaPopulationExp)
-const chinaPopulationExp = percentageOfWorld2("China", 1441)
-console.log(chinaPopulationExp)
-const germanyPopulationExp = percentageOfWorld2("Germany", 100000)
-console.log(germanyPopulationExp)
-
+const nigeriaPopulationExp = percentageOfWorld2("Nigeria", 120000);
+console.log(nigeriaPopulationExp);
+const chinaPopulationExp = percentageOfWorld2("China", 1441);
+console.log(chinaPopulationExp);
+const germanyPopulationExp = percentageOfWorld2("Germany", 100000);
+console.log(germanyPopulationExp);
 
 // ########## ARROW FUNCTIONS  ###########
-const percentageOfWorld3 = (country, population) => `${country} has ${population} people, so it's about ${(population / 7900) * 100}% of the world population`
+const percentageOfWorld3 = (country, population) =>
+  `${country} has ${population} people, so it's about ${
+    (population / 7900) * 100
+  }% of the world population`;
 
-const nigeriaPopulationArr = percentageOfWorld2("Nigeria", 120000)
-console.log(nigeriaPopulationArr)
-const chinaPopulationArr = percentageOfWorld2("China", 1441)
-console.log(chinaPopulationArr)
-const germanyPopulationArr = percentageOfWorld2("Germany", 100000)
-console.log(germanyPopulationArr)
-
+const nigeriaPopulationArr = percentageOfWorld2("Nigeria", 120000);
+console.log(nigeriaPopulationArr);
+const chinaPopulationArr = percentageOfWorld2("China", 1441);
+console.log(chinaPopulationArr);
+const germanyPopulationArr = percentageOfWorld2("Germany", 100000);
+console.log(germanyPopulationArr);
 
 // ########## FUNCTIONS CALLING OTHER FUNCTIONS  ###########
-function describePopulation (country, population){
-
-    const populationPercentage = percentageOfWorld1(population);
-    const countryPopulationStats = `${country} has ${population} people, which is about ${populationPercentage} of the world`
-    console.log(countryPopulationStats)
+function describePopulation(country, population) {
+  const populationPercentage = percentageOfWorld1(population);
+  const countryPopulationStats = `${country} has ${population} people, which is about ${populationPercentage} of the world`;
+  console.log(countryPopulationStats);
 }
 
-describePopulation("Nigeria", 1000000)
-describePopulation("China", 1200000)
-describePopulation("Germany", 90000)
-
+describePopulation("Nigeria", 1000000);
+describePopulation("China", 1200000);
+describePopulation("Germany", 90000);
 
 // function greetMe(name){
 //     return `Goodday, ${name}`
@@ -70,31 +70,34 @@ describePopulation("Germany", 90000)
 // }
 // const greetings = greetNow(prompt("WHat is your name?"))
 
-
 // ########## INTRO TO ARRAYS  ###########
 
-const populationArr = [120, 150, 22, 11]
+const populationArr = [120, 150, 22, 11];
 
-console.log(populationArr.length === 4)
-const percentages = [percentageOfWorld1(populationArr[0]), percentageOfWorld1(populationArr[1]), percentageOfWorld1(populationArr[2])]
-console.log(percentages)
+console.log(populationArr.length === 4);
+const percentages = [
+  percentageOfWorld1(populationArr[0]),
+  percentageOfWorld1(populationArr[1]),
+  percentageOfWorld1(populationArr[2]),
+];
+console.log(percentages);
 
 // ########## BASIC ARRAY OPERATIONS(OBJECTS)  ###########
-const neighbours = ["Niger", "Cameroun", "Chad" ]
-console.log(neighbours)
+const neighbours = ["Niger", "Cameroun", "Chad"];
+console.log(neighbours);
 
-neighbours.push("Utopia")
-console.log(neighbours)
+neighbours.push("Utopia");
+console.log(neighbours);
 
-neighbours.pop("Utopia")
-console.log(neighbours)
+neighbours.pop("Utopia");
+console.log(neighbours);
 
-if (!neighbours.includes("Germany")){
-    console.log('Probably not a central european country :D')
+if (!neighbours.includes("Germany")) {
+  console.log("Probably not a central european country :D");
 }
 
-neighbours[neighbours.indexOf("Chad")] = "Ghana"
-console.log(neighbours)
+neighbours[neighbours.indexOf("Chad")] = "Ghana";
+console.log(neighbours);
 
 // ########## INTRO TO OBJECTS  ###########
 
@@ -106,7 +109,6 @@ console.log(neighbours)
 //     neighbours: ["Niger", "Cameroun", "Chad" ]
 // }
 
-
 // ########## DOT VS BRACKET NOTATION  ###########
 // console.log(`'${myCountry.country} has ${myCountry.population} ${myCountry.language}-speaking people, ${myCountry.neighbours.length} neighbouring countries and a capital called ${myCountry.capital}'.`)
 
@@ -117,62 +119,100 @@ console.log(neighbours)
 // myCountry['population'] -= 2000000
 // console.log(myCountry.population)
 
-
 // ########## OBJECTS METHODS  ###########
 
 const myCountry = {
-    country: "Nigeria",
-    capital: "Abuja",
-    language: "English",
-    population: 120000000,
-    // neighbours: neighbours
-    neighbours: ["Niger", "Cameroun", "Chad" ],
-    describe: function(){
-        console.log(`${this.country} has ${this.population} ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`)
-    },
-    checkIsland: function(){
-        this.isIsland = this.neighbours.length === 0 ? true : false;
-    }
-}
-myCountry.describe()
-myCountry.checkIsland()
+  country: "Nigeria",
+  capital: "Abuja",
+  language: "English",
+  population: 120000000,
+  // neighbours: neighbours
+  neighbours: ["Niger", "Cameroun", "Chad"],
+  describe: function () {
+    console.log(
+      `${this.country} has ${this.population} ${this.language}-speaking people, ${this.neighbours.length} neighbouring countries and a capital called ${this.capital}.`
+    );
+  },
+  checkIsland: function () {
+    this.isIsland = this.neighbours.length === 0 ? true : false;
+  },
+};
+myCountry.describe();
+myCountry.checkIsland();
 console.log(myCountry);
 
-
-
 // ##########   ITERATION: THE FOR LOOP  ###########
-for (let voter = 1; voter <= 50; voter++){
-    console.log(`Voter number ${voter} is currently voting`)
+for (let voter = 1; voter <= 50; voter++) {
+  console.log(`Voter number ${voter} is currently voting`);
 }
 
 // ##########   LOOPING ARRAYS, BREAKING AND CONTINUING  ###########
-console.log("____I AM THE NEW POPULATION PERCENTAGE___________")
-const populationArray = [120, 150, 22, 11]
-const percentages2 = []
-for(let popPercent = 0; popPercent < populationArray.length; popPercent++){
-    percentages2.push(percentageOfWorld1(populationArray[popPercent]))
+console.log("____I AM THE NEW POPULATION PERCENTAGE___________");
+const populationArray = [120, 150, 22, 11];
+const percentages2 = [];
+for (let popPercent = 0; popPercent < populationArray.length; popPercent++) {
+  percentages2.push(percentageOfWorld1(populationArray[popPercent]));
 }
-console.log(percentages2)
+console.log(percentages2);
 
 // ##########   WHILE LOOP  ###########
-const populationArray1 = [120, 150, 22, 11]
-const percentages3 = []
-let popPercent = 0
-console.log("_______I AM WHILE LOOP________")
-while (popPercent < populationArray1.length){
-    percentages3.push(percentageOfWorld1(populationArray1[popPercent]))
-    popPercent++
-    
+const populationArray1 = [120, 150, 22, 11];
+const percentages3 = [];
+let popPercent = 0;
+console.log("_______I AM WHILE LOOP________");
+while (popPercent < populationArray1.length) {
+  percentages3.push(percentageOfWorld1(populationArray1[popPercent]));
+  popPercent++;
 }
-console.log(percentages3)
+console.log(percentages3);
 
 // ##########  LOOPING BACKWARDS AND LOOPS IN LOOPS ###########
-const listOfNeighbours = [['Canada', 'Mexico'], ['Spain'], ['Norway', 'Sweden', 'Russia']]
-console.log("_______I AM LOOP IN LOOPS________")
-for (let loopCountry1 = 0; loopCountry1 < listOfNeighbours.length; loopCountry1++)
-    for (let loopInloop = 0; loopInloop < listOfNeighbours[loopCountry1].length; loopInloop++)
-        console.log(`Neighbour: ${listOfNeighbours[loopCountry1][loopInloop]}`)
-   
-   
-   
-        
+const listOfNeighbours = [
+  ["Canada", "Mexico"],
+  ["Spain"],
+  ["Norway", "Sweden", "Russia"],
+];
+console.log("_______I AM LOOP IN LOOPS________");
+for (
+  let loopCountry1 = 0;
+  loopCountry1 < listOfNeighbours.length;
+  loopCountry1++
+)
+  for (
+    let loopInloop = 0;
+    loopInloop < listOfNeighbours[loopCountry1].length;
+    loopInloop++
+  )
+    console.log(`Neighbour: ${listOfNeighbours[loopCountry1][loopInloop]}`);
+
+// ##########  ASSIGNMENT ###########
+console.log("_______I AM THE ASSIGNMENT________");
+// Given two arrays of temperatures, join and calculate the amplitude of the temperatures
+// const array1 = [1, 2, 3, , 4, 5, "error", 6, 7, 8, 9, 10];
+// const array2 = ["error1", 21, 31, , 41, 51, 11, 61, 71, 81, 91, 10];
+
+const temperatureAltitude = (temp1, temp2) => {
+  const temp = temp1.concat(temp2);
+  console.log(temp);
+  let max = Number.NEGATIVE_INFINITY;
+  let min = Number.POSITIVE_INFINITY;
+  for (let i = 0; i < temp.length; i++) {
+    const curTemp = temp[i];
+    if (typeof curTemp !== "number") {
+      continue;
+    }
+    if (curTemp > max) {
+      max = curTemp;
+    }
+    if (curTemp < min) {
+      min = curTemp;
+    }
+  }
+  console.log(max, min);
+  return max - min;
+};
+const amplitude = temperatureAltitude(
+  ["error1", 21, 31, 41, 51, 11, 61, 71, 81, 91, 10],
+  [1, 2, 3, 4, 5, "error", 6, 7, 8, 9, 10]
+);
+console.log(amplitude);
