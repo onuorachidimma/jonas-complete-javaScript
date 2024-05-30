71c367cac1adf5d50b80b2a23c6bad035556f0aa
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-05-30 17:00:22 +0100

    implement matched input/guessed number/correct msg

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const Message = document.querySelector('.message');
+let GuessNum = Math.floor(Math.random() * 20) + 1;
+
+const checkMatch = () => {
+    const Guess = Number(document.querySelector('.guess').value);
+    console.log(GuessNum)
+    console.log(typeof Guess)
+    console.log(typeof GuessNum)
+    if (Guess > GuessNum) {
+        Message.textContent = "Too high!";
+    } else if (Guess < GuessNum) {
+        Message.textContent = "Too low!";
+    } else if (Guess === GuessNum) {
+        Message.textContent = "Correct!";
+        document.querySelector('.number').textContent = GuessNum;
+    }
+};
+
+document.querySelector('.check').addEventListener('click', checkMatch);
