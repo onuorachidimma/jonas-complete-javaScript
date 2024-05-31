1f3496de051faa63cb01282e6bbcd8950c0298d6
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-05-31 13:34:00 +0100

    reduce score on every failed attempt

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -10,12 +10,36 @@ const checkMatch = () => {
     console.log(typeof GuessNum)
     if (Guess > GuessNum) {
         Message.textContent = "Too high!";
+        document.querySelector('.score').textContent -= 1
     } else if (Guess < GuessNum) {
         Message.textContent = "Too low!";
+        document.querySelector('.score').textContent -= 1
     } else if (Guess === GuessNum) {
         Message.textContent = "Correct!";
-        document.querySelector('.number').textContent = GuessNum;
+        document.querySelector('.number').textContent = GuessNum
+        
+        let highScore = document.querySelector('.highscore').textContent = GuessNum
+        
+        
     }
 };
 
 document.querySelector('.check').addEventListener('click', checkMatch);
+
+const Again = () => {
+    let GuessNum = Math.floor(Math.random() * 20) + 1;
+
+    document.querySelector('.guess').value = ' ';
+    // GuessNum.textContent = "?"
+    document.querySelector('.number').textContent = '?'
+    document.querySelector('.message').textContent = 'Start guessing...';
+
+    // const highScore = document.querySelector('.highscore').textContent = GuessNum
+    if(GuessNum > highScore){
+        highScore = GuessNum
+    }else{
+        return highScore
+    }
+};
+
+document.querySelector('.again').addEventListener('click', Again);
