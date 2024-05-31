b5708a893a38f7d69c18f6d4e59071d124d0b182
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-05-31 14:40:07 +0100

    implement high score

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -2,12 +2,17 @@
 
 const Message = document.querySelector('.message');
 let GuessNum = Math.floor(Math.random() * 20) + 1;
+let highScore = document.querySelector('.highscore').textContent
+console.log(typeof highScore)
+// highScore = 0
 
 const checkMatch = () => {
     const Guess = Number(document.querySelector('.guess').value);
-    console.log(GuessNum)
-    console.log(typeof Guess)
-    console.log(typeof GuessNum)
+    // console.log(GuessNum)
+    // console.log(typeof Guess)
+    // console.log(typeof GuessNum)
+
+    // when guess is wrong
     if (Guess > GuessNum) {
         Message.textContent = "Too high!";
         document.querySelector('.score').textContent -= 1
@@ -19,10 +24,19 @@ const checkMatch = () => {
     } else if (Guess === GuessNum) {
         Message.textContent = "Correct!";
         document.querySelector('body').style.backgroundColor = 'green'   //change color of background if correct
+
         document.querySelector('.number').style.width = '30rem'   //change color of width of number if correct
+
         document.querySelector('.number').textContent = GuessNum
         
-        let highScore = document.querySelector('.highscore').textContent = GuessNum
+        // Implement high Score
+        if (GuessNum > document.querySelector('.highscore').textContent){
+            document.querySelector('.highscore').textContent = GuessNum
+        }else{
+            return document.querySelector('.highscore').textContent
+        }
+        
+        
         
         
     }
@@ -33,19 +47,13 @@ document.querySelector('.check').addEventListener('click', checkMatch);
 const Again = () => {
     document.querySelector('body').style.backgroundColor = '#222' //revert to old color
     document.querySelector('.number').style.width = '15rem' //revert to old width of number
-    let GuessNum = Math.floor(Math.random() * 20) + 1;
+    GuessNum = Math.floor(Math.random() * 20) + 1;
 
     document.querySelector('.guess').value = ' ';
     // GuessNum.textContent = "?"
     document.querySelector('.number').textContent = '?'
     document.querySelector('.message').textContent = 'Start guessing...';
 
-    // const highScore = document.querySelector('.highscore').textContent = GuessNum
-    if(GuessNum > highScore){
-        highScore = GuessNum
-    }else{
-        return highScore
-    }
 };
 
 document.querySelector('.again').addEventListener('click', Again);
