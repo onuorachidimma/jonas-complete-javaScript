0ea7a959f37e98522bf0043ede4cd24a4e9cd684
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-06-01 19:38:07 +0100

    Cleanup code and implement DRY principle

--- 05-Guess-My-Number/starter/script.js
+++ 05-Guess-My-Number/starter/script.js
@@ -7,63 +7,30 @@ console.log(typeof highScore)
 let score = 20
 
 const checkMatch = () => {
+    // WHEN GUESS IS CORRECT
     const Guess = Number(document.querySelector('.guess').value);
-    // console.log(GuessNum)
-    // console.log(typeof Guess)
-    // console.log(typeof GuessNum)
-
-    // when guess is wrong
-
-
     if (Guess === GuessNum) {
         Message.textContent = "Correct!";
         document.querySelector('body').style.backgroundColor = 'green'   //change color of background if correct
-
         document.querySelector('.number').style.width = '30rem'   //change color of width of number if correct
-
         document.querySelector('.number').textContent = GuessNum
         
-        // Implement high Score
+        // IMPLEMENT HIGH SCORE
         if (document.querySelector('.score').textContent > document.querySelector('.highscore').textContent){
             document.querySelector('.highscore').textContent = document.querySelector('.score').textContent
         }else{
             return document.querySelector('.highscore').textContent
         }
         
-        
-        
-        
-    }else if (Guess > GuessNum) {
+    } else{  // WHEN GUESS IS WRONG
         if(document.querySelector('.score').textContent > 1){
-            Message.textContent = "Too High!";
+            Message.textContent = Guess > GuessNum ? "Too High!" : "Too Low";   //IF GUESS IS GREATER OR LESS THAN GUESSNUM
             document.querySelector('.score').textContent -= 1
         }else{
             document.querySelector('.score').textContent = "0"
             Message.textContent = "Game Over!"
         }
-
-        // Message.textContent = "Too high!";
-        // document.querySelector('.score').textContent -= 1
-        
-        // if (document.querySelector('.score').textContent == 0){
-        //     Message.textContent = "Game Over!"
-        // }
-    } else if (Guess < GuessNum) {
-        if(document.querySelector('.score').textContent > 1){
-            Message.textContent = "Too low!";
-            document.querySelector('.score').textContent -= 1
-        }else{
-            document.querySelector('.score').textContent = "0"
-            Message.textContent = "Game Over!"
-        }
-        
-
-        // if (document.querySelector('.score').textContent == 0){
-            
-        // }
-
-        // when guess is correct
-    } 
+    }
 };
 
 document.querySelector('.check').addEventListener('click', checkMatch);
@@ -72,14 +39,10 @@ const Again = () => {
     document.querySelector('body').style.backgroundColor = '#222' //revert to old color
     document.querySelector('.number').style.width = '15rem' //revert to old width of number
     GuessNum = Math.floor(Math.random() * 20) + 1;
-
     document.querySelector('.guess').value = ' ';
-    // GuessNum.textContent = "?"
     document.querySelector('.number').textContent = '?'
     document.querySelector('.message').textContent = 'Start guessing...';
-
     document.querySelector('.score').textContent = "20"
-
 };
 
 document.querySelector('.again').addEventListener('click', Again);
