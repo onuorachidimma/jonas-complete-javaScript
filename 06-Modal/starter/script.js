bba55bbb5359c107fac6202fddcbc1677c8aaac8
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-06-03 22:46:52 +0100

    loop through each element with same classname

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,23 +1,25 @@
 'use strict';
-const showModal = document.querySelector('.show-modal');
+const showModal = document.querySelectorAll('.show-modal');
 const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.close-modal');
 const hiddenModal = document.querySelector('.hidden');
 const overlay = document.querySelector('.overlay');
 
-showModal.addEventListener('click', function openModal() {
+function openModal() {
   hiddenModal.style.display = 'block';
-//   modal.style.display = 'block';
+  //   modal.style.display = 'block';
   overlay.style.display = 'block';
   console.log('I just got clicked');
-});
+}
 
-closeModal.addEventListener('click', function killModal() {
+function killModal() {
   hiddenModal.style.display = 'none';
-    overlay.style.display = 'none';
+  overlay.style.display = 'none';
   console.log('I just got clicked');
+}
+
+showModal.forEach(element => {
+  element.addEventListener('click', openModal);
 });
 
-//  const openModal = () => {
-
-//  }
+hiddenModal.addEventListener('click', killModal);
