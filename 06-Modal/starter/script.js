078f5e56dc625d56d015fec7e4b47f44c8a01be0
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-06-03 22:27:27 +0100

    show and close modal

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -1,1 +1,23 @@
 'use strict';
+const showModal = document.querySelector('.show-modal');
+const modal = document.querySelector('.modal');
+const closeModal = document.querySelector('.close-modal');
+const hiddenModal = document.querySelector('.hidden');
+const overlay = document.querySelector('.overlay');
+
+showModal.addEventListener('click', function openModal() {
+  hiddenModal.style.display = 'block';
+//   modal.style.display = 'block';
+  overlay.style.display = 'block';
+  console.log('I just got clicked');
+});
+
+closeModal.addEventListener('click', function killModal() {
+  hiddenModal.style.display = 'none';
+    overlay.style.display = 'none';
+  console.log('I just got clicked');
+});
+
+//  const openModal = () => {
+
+//  }
