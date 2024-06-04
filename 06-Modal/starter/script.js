9177b0d7a04969e8c98e105006d6b925f2d81b50
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-06-04 19:29:25 +0100

    open/close modal by selecting class with classList

--- 06-Modal/starter/script.js
+++ 06-Modal/starter/script.js
@@ -4,7 +4,7 @@ const modal = document.querySelector('.modal');
 const closeModal = document.querySelector('.close-modal');
 const hiddenModal = document.querySelector('.hidden');
 const overlay = document.querySelector('.overlay');
-
+/*
 function openModal() {
   hiddenModal.style.display = 'block';
   //   modal.style.display = 'block';
@@ -23,3 +23,22 @@ showModal.forEach(element => {
 });
 
 hiddenModal.addEventListener('click', killModal);
+*/
+
+function openModal() {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+  console.log('I just got clicked');
+}
+
+for (let i = 0; i < showModal.length; i++) {
+  showModal[i].addEventListener('click', openModal);
+}
+
+function killModal() {
+  modal.classList.add('hidden');
+  overlay.classList.add('hidden');
+  console.log('I just got clicked');
+}
+
+closeModal.addEventListener('click', killModal);
