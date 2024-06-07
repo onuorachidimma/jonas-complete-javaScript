02d8d075442a649b2f87f2753bc7389b93e8595f
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-06-07 15:59:19 +0100

    create function to roll dice

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -15,18 +15,17 @@
     // console.log('I am clicked')
  }newGame.addEventListener('click', resetGame)
 
+ //  newGame.addEventListener('click', function resetGame(){
+//     for (let i = 0; i < score.length && i < currentScore.length; i++){
+//         score[i].textContent = '0'
+//         currentScore[i].textContent = '0'
+//     }
+//     rollingDice.style.display = 'none'
+//  })
 
- // ROLL DICE
-
- const diceArr = ['./dice-1.png', './dice-2.png', './dice-3.png', './dice-4.png', './dice-5.png', './dice-6.png']
-//  for (let arr = 0; arr < diceArr.length - 1; arr++){
-    
-// function randomArray(arr){
-//     const randomDice = Math.floor(Math.random() * arr.length)
-//     return arr[randomDice]
-// }
-// const newArr = randomArray(diceArr)
-// console.log(newArr)
+ 
+// ROLL DICE
+const diceArr = ['./dice-1.png', './dice-2.png', './dice-3.png', './dice-4.png', './dice-5.png', './dice-6.png']
 
 rollDice.addEventListener('click', function(){
     function randomArray(arr){
@@ -35,7 +34,7 @@ rollDice.addEventListener('click', function(){
     }
     const newArr = randomArray(diceArr)
     console.log(newArr)
-    // console.log(newArr, "I am clickes")
+    // console.log(newArr, "I am clicked")
     rollingDice.style.display = 'block'
     diceArr.forEach(dice => {
         rollingDice.src = newArr
@@ -43,11 +42,4 @@ rollDice.addEventListener('click', function(){
     
 })
     
-//  }
-//  newGame.addEventListener('click', function resetGame(){
-//     for (let i = 0; i < score.length && i < currentScore.length; i++){
-//         score[i].textContent = '0'
-//         currentScore[i].textContent = '0'
-//     }
-//     rollingDice.style.display = 'none'
-//  })
+
