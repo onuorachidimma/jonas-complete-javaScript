7710615890f4c2d1f2032bacd56f29765a48f3c0
Chidimma Onuora <onuorachidimma4@gmail.com>
2024-06-07 15:55:41 +0100

    roll dice

--- 07-Pig-Game/starter/script.js
+++ 07-Pig-Game/starter/script.js
@@ -3,6 +3,7 @@
  const score = document.querySelectorAll('.score')
  const currentScore = document.querySelectorAll('.current-score')
  const rollingDice = document.querySelector('.dice')
+ const rollDice = document.querySelector('.btn--roll')
 
 // FUNCTION TO RESET GAME
  function resetGame(){
@@ -14,6 +15,35 @@
     // console.log('I am clicked')
  }newGame.addEventListener('click', resetGame)
 
+
+ // ROLL DICE
+
+ const diceArr = ['./dice-1.png', './dice-2.png', './dice-3.png', './dice-4.png', './dice-5.png', './dice-6.png']
+//  for (let arr = 0; arr < diceArr.length - 1; arr++){
+    
+// function randomArray(arr){
+//     const randomDice = Math.floor(Math.random() * arr.length)
+//     return arr[randomDice]
+// }
+// const newArr = randomArray(diceArr)
+// console.log(newArr)
+
+rollDice.addEventListener('click', function(){
+    function randomArray(arr){
+        const randomDice = Math.floor(Math.random() * arr.length)
+        return arr[randomDice]
+    }
+    const newArr = randomArray(diceArr)
+    console.log(newArr)
+    // console.log(newArr, "I am clickes")
+    rollingDice.style.display = 'block'
+    diceArr.forEach(dice => {
+        rollingDice.src = newArr
+    })
+    
+})
+    
+//  }
 //  newGame.addEventListener('click', function resetGame(){
 //     for (let i = 0; i < score.length && i < currentScore.length; i++){
 //         score[i].textContent = '0'
